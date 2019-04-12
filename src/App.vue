ad12145d063acc35b2f40342337a18bc8e899cc5
sunxiangfan <13111110495@163.com>
2019-04-12 16:12:14 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<!--<style>-->
+<!--  .app-container .small-padding .cell{-->
+<!--    display: flex;-->
+<!--    justify-content: flex-start;-->
+<!--  }-->
+<!--</style>-->
